8ebe9e35c3215de5ee97812aaf89a02b8d269e68
jasvazquez <jasvazquez@gmail.com>
2017-02-06 18:51:59 +0100

    Devolvemos como string el rsdo. de un comando del sistema. Creamos método 'mock' que simula las llamadas a SpeedTest. Gestionamos las distintas líneas de la respuesta obtenida de invocar un comando del sistema.

--- testVelocidad.py
+++ testVelocidad.py
@@ -8,7 +8,15 @@ def invocarComando(comando, params, **sustituciones):
 	vals=shlex.split(params)
 	if len(sustituciones)>0:
 		vals=[x.format(**sustituciones) for x in vals]	
-	convert = subprocess.check_call([comando]+vals)
+	convert = subprocess.check_output([comando]+vals)
 	return str(convert)
 
-invocarComando("speedtest","--list")
+def mock_invocarComando(comando, params, **sustituciones):
+	return 	"Ping: 88.273 ms \n"\
+	"Download: 9.23 Mbit/s \n" \
+	"Upload: 3.01 Mbit/s"
+
+rsdo=mock_invocarComando("speedtest"," --simple --server 7385")
+
+for r in rsdo.rstrip().split('\n'):
+	print ">>> {L}".format(L=r)
