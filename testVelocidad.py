ab8b783e40bbe599700645d8285831233aab081a
jasvazquez <jasvazquez@gmail.com>
2017-02-06 19:15:19 +0100

    Se añade BD y código para insertar entradas en ella.

--- testVelocidad.py
+++ testVelocidad.py
@@ -3,6 +3,32 @@
 
 import ushlex as shlex
 import os, sys, subprocess
+import sqlite3
+from collections import namedtuple
+
+def getDirectorioEjecucionScript():
+	return os.path.dirname(os.path.abspath(__file__))
+	
+## FICHERO BD (SqLite) con datos de la aplicación
+FICHERO_BD=getDirectorioEjecucionScript()+'/velocidades.db'
+	
+def namedtuple_factory(cursor, row):
+    """
+    Uso:
+    con.row_factory = namedtuple_factory
+    """
+    fields = [col[0] for col in cursor.description]
+    Row = namedtuple("Row", fields)
+    return Row(*row)
+    
+def db_execute(sql, datos):
+	
+	con = sqlite3.connect(FICHERO_BD)
+	con.row_factory = namedtuple_factory
+	cur = con.cursor()
+	
+	cur.execute(sql, datos)	
+	con.commit()
 
 def invocarComando(comando, params, **sustituciones):
 	vals=shlex.split(params)
@@ -16,7 +42,16 @@ def mock_invocarComando(comando, params, **sustituciones):
 	"Download: 9.23 Mbit/s \n" \
 	"Upload: 3.01 Mbit/s"
 
+def insertarMedicion(ping, bajada, subida):
+	# TODO poner hora del sistema
+	t_ins=("2017.02.06 12:01:02",ping,bajada,subida)
+	c=db_execute('insert into anotacion values (?,?,?,?)',t_ins)
+
+insertarMedicion("73 ms","12.72 Mbits/s","2.34 Mbits/s")	
+quit()
+
 rsdo=mock_invocarComando("speedtest"," --simple --server 7385")
 
 for r in rsdo.rstrip().split('\n'):
-	print ">>> {L}".format(L=r)
+	valor=r.split(":")[1].strip()
+	print ">>> {L}".format(L=valor)
