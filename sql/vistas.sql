add9b2f6870fa5ac503e497d198d0ea33b4f1590
jasvazquez <jasvazquez@gmail.com>
2017-02-11 19:35:40 +0100

    Creamos método mock que simula la obtención de las estadísticas de velocidad semanales. Implantamos la creación de gráficas SVG con los datos obtenidos.

---
+++ sql/vistas.sql
@@ -0,0 +1,70 @@
+select max(ping) max_ping, avg(ping) ping, min(ping) min_ping, avg(bajada) bajada, avg(subida) subida from anotacion;
+select * from anotacion order by ping desc;
+
+select 
+	max(ping) max_ping, avg(ping) ping, min(ping) min_ping, 
+	max(bajada) max_bajada, avg(bajada) bajada, min(bajada) min_bajada, 
+	max(subida) max_subida, avg(subida) subida, min(subida) min_subida
+from (
+	select 
+		cast(ping as decimal) ping,
+		cast(bajada as decimal) bajada,
+		cast(subida as decimal) subida
+	from anotacion
+);
+
+select fecha  from anotacion where cast(fecha as datetime)>=strftime(date('now'));
+select date('now');
+
+/* [https://goo.gl/vXiUZZ] Manejo de fechas en SQLITE*/
+
+SELECT date(fecha) dia, strftime('%H',fecha) hora,* from anotacion where fecha>=datetime('now','start of day') and fecha<=datetime('now','start of day','+24 hours','-1 seconds'); 
+
+// Datos de la semana que ha pasado 
+
+select 
+	dia,
+	max(ping) max_ping, avg(ping) avg_ping, min(ping) min_ping, 
+	max(bajada) max_bajada, avg(bajada) avg_bajada, min(bajada) min_bajada, 
+	max(subida) max_subida, avg(subida) avg_subida, min(subida) min_subida 
+from 
+(
+	select 
+		date(fecha) dia, 
+		cast(ping as decimal) ping,
+		cast(bajada as decimal) bajada,
+		cast(subida as decimal) subida
+	from anotacion 
+	where 
+		fecha>=datetime('now','start of day','-4 days') and fecha<=datetime('now','start of day')
+)
+group by dia;
+
+select 
+	
+	case cast (strftime('%w', 'now') as integer)
+		  when 0 then 'D'
+		  when 1 then 'L'
+		  when 2 then 'M'
+		  when 3 then 'X'
+		  when 4 then 'J'
+		  when 5 then 'V'
+		  else 'S' 
+	end  as dia;
+	
+ select 
+	case strftime('%m', 'now') 
+		when '01' then 'Ene' 
+		when '02' then 'Feb' 
+		when '03' then 'Mar' 
+		when '04' then 'Abr' 
+		when '05' then 'May' 
+		when '06' then 'Jun' 
+		when '07' then 'Jul' 
+		when '08' then 'Ago' 
+		when '09' then 'Sep'
+		when '10' then 'Oct' 
+		when '11' then 'Nov' 
+		when '12' then 'Dic' 
+		else '' 
+	end as month ;
--- testVelocidad.py
+++ testVelocidad.py
@@ -9,6 +9,9 @@ from collections import namedtuple
 
 import simplejson as json
 
+import pygal
+import datetime
+
 def getDirectorioEjecucionScript():
 	return os.path.dirname(os.path.abspath(__file__))
 	
@@ -49,6 +52,15 @@ def invocarComando(comando, params, **sustituciones):
 		
 	return json.loads(rsdo)
 
+def getAnotacionesSemanales():
+	# TODO implementar 'select' a la BD
+	return 	[
+				{ "dia" : """2017-02-07""", "max_ping" : """113.771""", "avg_ping" : """90.6546666666667""", "min_ping" : """81.384""", "max_bajada" : """24.4781403170456""", "avg_bajada" : """20.2730597238812""", "min_bajada" : """13.2055103784335""", "max_subida" : """3.09292332230586""", "avg_subida" : """2.96405310906295""", "min_subida" : """2.81179095514377""" },
+				{ "dia" : """2017-02-08""", "max_ping" : """114.991""", "avg_ping" : """91.9907777777778""", "min_ping" : """78.938""", "max_bajada" : """46.0563639650376""", "avg_bajada" : """24.0650956913638""", "min_bajada" : """6.80702860741628""", "max_subida" : """3.35004730077555""", "avg_subida" : """2.87022332932429""", "min_subida" : """2.41445603005469""" },
+				{ "dia" : """2017-02-09""", "max_ping" : """120.304""", "avg_ping" : """88.3216666666667""", "min_ping" : """78.147""", "max_bajada" : """45.703996606504""", "avg_bajada" : """26.3445890659491""", "min_bajada" : """7.96970023204263""", "max_subida" : """3.35515793642163""", "avg_subida" : """2.84024639108092""", "min_subida" : """1.99096124551157""" },
+				{ "dia" : """2017-02-10""", "max_ping" : """165.028""", "avg_ping" : """95.706""", "min_ping" : """75.985""", "max_bajada" : """45.8590445983241""", "avg_bajada" : """22.0138659418411""", "min_bajada" : """8.06844512621522""", "max_subida" : """3.26924579763993""", "avg_subida" : """2.92126476368912""", "min_subida" : """2.76515263362812""" },
+			]
+
 def insertarMedicion(ping, bajada, subida, servidor):
 	
 	# IDEA patrón decorador
@@ -62,6 +74,37 @@ def setConfig(nombFichero):
 	global config
 	with open(nombFichero) as data_file:    
 		config = json.load(data_file)
+
+dia=[]
+max=[]
+min=[]
+avg=[]
+
+anotaciones=getAnotacionesSemanales()
+
+for a in anotaciones:
+	
+	# IDEA traducir día de la semana (Monday, Tuesday, ...) al castellano
+	
+	dia.append(datetime.datetime.strptime(a['dia'], "%Y-%m-%d").strftime("%a %d/%b"))
+	max.append(float(a['max_bajada']))
+	min.append(float(a['min_bajada']))
+	avg.append(float(a['avg_bajada']))
+
+
+from pygal.style import CleanStyle
+
+chart = pygal.Line(fill=True)
+chart.title = 'BAJADA semanal'
+chart.x_labels = dia #map(str, range(2002, 2013))
+chart.add('Max', max)
+chart.add('Avg', avg)
+chart.add('Min', min)
+chart.render()
+
+chart.render_to_file('chart.svg')  
+
+quit()
 		
 setConfig('./config.json')
 r=invocarComando("speedtest"," --json")
